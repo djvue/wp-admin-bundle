1fcd0319d4816baf1f3aae6e8e55023eb4d5f4ce
djvue <d.vahlov@yandex.ru>
2021-03-29 00:10:51 +0300

    fix(replace config prefix)

--- src/DependencyInjection/WpAdminExtension.php
+++ src/DependencyInjection/WpAdminExtension.php
@@ -3,9 +3,9 @@
 
 namespace Djvue\WpAdminBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class WpAdminExtension extends Extension
@@ -15,14 +15,14 @@ class WpAdminExtension extends Extension
         $configuration = $this->getConfiguration($configs, $container);
         $config = $this->processConfiguration($configuration, $configs);
 
-        $container->setParameter('wp.host', $config['host']);
-        $container->setParameter('wp.table_prefix', $config['table_prefix']);
-        $container->setParameter('wp.database', $config['database']);
-        $container->setParameter('wp.namespaces.configurator', $config['namespaces']['configurator']);
-        $container->setParameter('wp.namespaces.field_group', $config['namespaces']['field_group']);
-        $container->setParameter('wp.page_templates_path', $config['page_templates_path']);
+        $container->setParameter('wp_admin.host', $config['host']);
+        $container->setParameter('wp_admin.table_prefix', $config['table_prefix']);
+        $container->setParameter('wp_admin.database', $config['database']);
+        $container->setParameter('wp_admin.namespaces.configurator', $config['namespaces']['configurator']);
+        $container->setParameter('wp_admin.namespaces.field_group', $config['namespaces']['field_group']);
+        $container->setParameter('wp_admin.page_templates_path', $config['page_templates_path']);
 
-        $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__) . '/Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__).'/Resources/config'));
         $loader->load('services.yaml');
     }
 
